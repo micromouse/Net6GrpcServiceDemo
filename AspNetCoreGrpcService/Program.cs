1bbcae445bea7bc5a4a97e04cd079cfdf2637b01
eggeggak <eggeggak@vip.qq.com>
2021-12-14 17:55:10 +0800

    add consolegrpcclient project

--- AspNetCoreGrpcService/Program.cs
+++ AspNetCoreGrpcService/Program.cs
@@ -1,16 +1,25 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Net6GrpcServiceDemo.AspNetCoreGrpcService.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication
+    .CreateBuilder(args);
+builder.WebHost
+    .ConfigureKestrel(x =>
+    {
+        x.ListenAnyIP(80, x1 => x1.Protocols = HttpProtocols.Http1AndHttp2);
+        x.ListenAnyIP(443, x1 => x1.Protocols = HttpProtocols.Http2);
+    })
+    .ConfigureServices(x =>
+    {
+        x.AddGrpc();
+    });
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
-builder.Services.AddGrpc();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
---
+++ ConsoleGrpcClient/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleGrpcClient;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args).Build();
+
+var httpClientHandler = new HttpClientHandler();
+httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, httpClientHandler) {
+    HttpVersion = new Version(1, 1)
+};
+
+var configuration = host.Services.GetService<IConfiguration>();
+using var channel = GrpcChannel.ForAddress($"http://192.168.1.112:{configuration["Grpc_ServicePort"]}", new GrpcChannelOptions { HttpHandler = httpHandler });
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Hello From ConsoleGrpcClient " });
+Console.WriteLine($"Greeting:{reply.Message}");
+
+await host.RunAsync();
+
