6690062777b0021e69f6b1a9ffdad31fd1e5abe5
eggeggak <eggeggak@vip.qq.com>
2021-12-17 14:49:19 +0800

    update consolegrpcclient

--- ConsoleGrpcClient/Program.cs
+++ ConsoleGrpcClient/Program.cs
@@ -7,11 +7,20 @@ using Microsoft.Extensions.Hosting;
 
 using var host = Host.CreateDefaultBuilder(args).Build();
 
-var configuration = host.Services.GetService<IConfiguration>();
-using var channel = GrpcChannel.ForAddress($"http://aspnetcoregrpcservice:{configuration["Grpc_ServicePort"]}");
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "Hello From ConsoleGrpcClient " });
-Console.WriteLine($"收到服务端消息:{reply.Message}");
-
+TimingRequestSayHello();
 await host.RunAsync();
 
+/// <summary>
+/// 定时请求<see cref="Greeter.GreeterClient.SayHelloAsync(HelloRequest, Grpc.Core.CallOptions)(HelloRequest)"/>
+/// </summary>
+async Task TimingRequestSayHello() {
+    Thread.Sleep(10000);
+
+    var configuration = host.Services.GetService<IConfiguration>();
+    using var channel = GrpcChannel.ForAddress($"http://aspnetcoregrpcservice:{configuration["Grpc_ServicePort"]}");
+    var client = new Greeter.GreeterClient(channel);
+    var reply = await client.SayHelloAsync(new HelloRequest { Name = "Hello From ConsoleGrpcClient " });
+    Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:sss}]收到服务端消息:{reply.Message}");
+
+    await TimingRequestSayHello();
+}
